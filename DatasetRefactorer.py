aaa7a489a597d4aa45ee10ffb14202449843662d
abhash 81iq <32843407+ZeoZagart@users.noreply.github.com>
2020-02-10 20:54:51 +0530

    Add code to refactor dataset
    use multiple processes to convert a batch of 5k examples to a refactored set of examples

---
+++ DatasetRefactorer.py
@@ -0,0 +1,36 @@
+import Constants
+from Example import Example, ExampleCreator
+from tqdm import tqdm
+import json
+import multiprocessing 
+import gc
+
+def worker(data, writer, lock) : 
+	result_set = []
+	for item in data : 
+		result_set += ExampleCreator.train_item_to_examples(item)
+	lock.acquire()
+	for item in result_set : 
+		writer.write(json.dumps(item.to_dict()) + '\n')
+	lock.release()
+
+
+dataset = []
+queue   = []
+op_file = open(Constants.train_op_path,"w")
+
+lock = multiprocessing.Lock()
+with open(Constants.train_file_path, 'r') as train_file : 
+	for i, line in tqdm(enumerate(train_file)) : 
+		dataset.append(json.loads(line))
+		if i%5000 == 0 : 
+			work = multiprocessing.Process(target=worker, args=(dataset, op_file, lock))
+			work.start()
+			queue.append(work)
+			dataset = []
+			gc.collect()
+
+for work in tqdm(queue) : 
+	work.join()
+
+op_file.close()
