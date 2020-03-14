e914b132c51d38ab10a3a363ec345734908be3ed
abhash 81iq <32843407+ZeoZagart@users.noreply.github.com>
2020-03-15 01:29:45 +0530

    Delete LossFn.py

--- Refactorer/LossFn.py
+++
@@ -1,7 +0,0 @@
-import torch
-
-class LossFn: 
-    loss_AT = torch.nn.CrossEntropyLoss(weight=torch.FloatTensor([0.1, 0.4, 0.5]))
-    loss_start = torch.nn.BCEWithLogitsLoss()
-    loss_end = torch.nn.BCEWithLogitsLoss()
-    loss_yes_no = torch.nn.CrossEntropyLoss(weight=torch.FloatTensor([0.1, 0.45, 0.45]))
