63f1b280a8164fb986bc901e2f4395a140bd0c35
abhash.ya <2015csb1001@iitrpr.ac.in>
2020-03-29 02:12:51 +0530

    Fix argument #3 weight(Loss fn class balance weights) is in CPU expected in device

--- Model/LossFn.py
+++ Model/LossFn.py
@@ -1,7 +1,8 @@
 import torch
 
 class LossFn: 
-    loss_AT = torch.nn.CrossEntropyLoss(weight=torch.FloatTensor([0.1, 0.4, 0.5]))
-    loss_start = torch.nn.BCEWithLogitsLoss()
-    loss_end = torch.nn.BCEWithLogitsLoss()
-    loss_yes_no = torch.nn.CrossEntropyLoss(weight=torch.FloatTensor([0.1, 0.45, 0.45]))
+	def __init__(self, device)
+	    ans_type = torch.nn.CrossEntropyLoss(weight=torch.FloatTensor([0.1, 0.4, 0.5]).to(device))
+    	start = torch.nn.BCEWithLogitsLoss()
+    	end = torch.nn.BCEWithLogitsLoss()
+    	yes_no = torch.nn.CrossEntropyLoss(weight=torch.FloatTensor([0.1, 0.45, 0.45]).to(device))
