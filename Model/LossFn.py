665ea55f0e674337f26eed4dffd65def410acf39
abhash.ya <2015csb1001@iitrpr.ac.in>
2020-03-29 11:57:14 +0530

    Update LossFn.py

--- Model/LossFn.py
+++ Model/LossFn.py
@@ -1,8 +1,8 @@
 import torch
 
 class LossFn: 
-	def __init__(self, device)
-	    ans_type = torch.nn.CrossEntropyLoss(weight=torch.FloatTensor([0.1, 0.4, 0.5]).to(device))
-    	start = torch.nn.BCEWithLogitsLoss()
-    	end = torch.nn.BCEWithLogitsLoss()
-    	yes_no = torch.nn.CrossEntropyLoss(weight=torch.FloatTensor([0.1, 0.45, 0.45]).to(device))
+	def __init__(self, device):
+	    self.ans_type = torch.nn.CrossEntropyLoss(weight=torch.FloatTensor([0.1, 0.4, 0.5]).to(device))
+    	self.start = torch.nn.BCEWithLogitsLoss()
+    	self.end = torch.nn.BCEWithLogitsLoss()
+    	self.yes_no = torch.nn.CrossEntropyLoss(weight=torch.FloatTensor([0.1, 0.45, 0.45]).to(device))
