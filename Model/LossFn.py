3f49675bc3ca48fb6732022f78997f740dc9b61c
abhash.ya <2015csb1001@iitrpr.ac.in>
2020-03-29 14:52:23 +0530

    Move bert encoder out from dataset to train file
    Move to train to have more fine handling of bert encoder since it is one of the most power hunger task

--- Model/LossFn.py
+++ Model/LossFn.py
@@ -2,7 +2,7 @@ import torch
 
 class LossFn: 
 	def __init__(self, device):
-	    self.ans_type = torch.nn.CrossEntropyLoss(weight=torch.FloatTensor([0.1, 0.4, 0.5]).to(device))
-    	self.start = torch.nn.BCEWithLogitsLoss()
-    	self.end = torch.nn.BCEWithLogitsLoss()
-    	self.yes_no = torch.nn.CrossEntropyLoss(weight=torch.FloatTensor([0.1, 0.45, 0.45]).to(device))
+		self.ans_type = torch.nn.CrossEntropyLoss(weight=torch.FloatTensor([0.1, 0.4, 0.5]).to(device))
+		self.start = torch.nn.BCEWithLogitsLoss()
+		self.end = torch.nn.BCEWithLogitsLoss()
+		self.yes_no = torch.nn.CrossEntropyLoss(weight=torch.FloatTensor([0.1, 0.45, 0.45]).to(device))
--- Model/datasetutils.py
+++ Model/datasetutils.py
@@ -11,9 +11,8 @@ def get_dataset(device = torch.device("cpu"), num_workers = 0) :
 	with open(Constants.train_path,"r") as train_file : 
 		dataset = [TrainExample(**(json.loads(line))) for line in tqdm(train_file)]
 
-	if num_workers != 0 and device != torch.device('cpu') : 
-		print ("Error : num_workers != 0 for gpu device not allowed")  
-	nqdataset = NQDataset(dataset, device)
+
+	nqdataset = NQDataset(dataset)
 	train_len = int(len(nqdataset)*0.95)
 	valid_len = len(nqdataset) - train_len
 	trainData, valData = torch.utils.data.random_split(nqdataset, [train_len, valid_len] )
